48a91d9fe36ab4b6b28c938b4623d1b65697f3b3
Faizrakhman Alikhan <240381@astanait.edu.kz>
2024-12-25 02:47:22 +0500

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Alikhan", "Faizrakhman", 18, true);
+        Person person2 = new Person("Asset", "Issaev", 18, true);
+        Person person3 = new Person("Akbota", "Arai", 17, false);
+
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+    }
+}
